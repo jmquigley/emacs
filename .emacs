b3e079912a9177d1298089d141bc467815b23fbe
James Quigley <jmquigley@outlook.com>
2014-10-20 23:31:37 -0500

    * Initial creation of the el-get environment for Emacs.

---
+++ .emacs
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/settings")
+(setq plugin-path "~/.emacs.d/el-get/")
+(setq el-get-user-package-directory "~/emacs.d/init")
+
+(require 'el-get-settings)
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+NOW=$(date +"%Y%m%d%H%M%S")
+
+# Run from the home location of the script
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+pushd ${DIR} > /dev/null
+
+# Copies this package into home.  NO BACKUP
+cp -R .emacs.d/* ~/.emacs.d
+[ -f ~/.emacs ] && rm ~/.emacs
+cp .emacs ~/.
+
+popd > /dev/null
+
