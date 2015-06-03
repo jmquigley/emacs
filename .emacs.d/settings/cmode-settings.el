89914838d466134f91bc42bf4a27a309635211d7
James Quigley <jmquigley@outlook.com>
2015-06-03 00:55:46 -0500

    * Fixed the tabs to spaces and indent in C mode

--- .emacs.d/settings/cmode-settings.el
+++ .emacs.d/settings/cmode-settings.el
@@ -8,6 +8,10 @@
     (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
     (setq tab-width 4)
     (setq indent-tabs-mode t)
+
+    (setq c-basic-indent 4)
+    (setq-default indent-tabs-mode nil)
+
     (ggtags-mode 1)
 ))
 
