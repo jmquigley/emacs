7a3eab7efd04188bebda42424e51d52c706fe885
James Quigley <jmquigley@outlook.com>
2015-06-03 00:59:39 -0500

    * Forgot to remove old C mode setting for tabs

--- .emacs.d/settings/cmode-settings.el
+++ .emacs.d/settings/cmode-settings.el
@@ -7,7 +7,6 @@
     (setq c-indent-level 4)
     (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
     (setq tab-width 4)
-    (setq indent-tabs-mode t)
 
     (setq c-basic-indent 4)
     (setq-default indent-tabs-mode nil)
