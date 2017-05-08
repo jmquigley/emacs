4e564355bd69c9a8ca30ac84cab24c3caf412e7d
James Quigley <jmquigley@outlook.com>
2017-05-08 15:21:09 -0500

    Adds a save hook to remove trailing whitespace on save

--- .emacs.d/settings/buffer-settings.el
+++ .emacs.d/settings/buffer-settings.el
@@ -1,4 +1,7 @@
 (add-hook 'find-file-hook 'update-buffer-list)
 (add-hook 'after-save-hook 'update-buffer-list)
+(add-hook 'before-save-hook '(lambda()
+    (delete-trailing-whitespace)
+))
 
 (provide 'buffer-settings)
