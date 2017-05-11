7ef233bf103213ae6a417fe0175d109d390a2b71
James Quigley <jmquigley@outlook.com>
2017-05-11 13:10:28 -0500

    Adds sh-mode hooks for some bash startup files

--- .emacs.d/settings/shell-settings.el
+++ .emacs.d/settings/shell-settings.el
@@ -1,3 +1,11 @@
+(add-to-list 'auto-mode-alist '("\\.alias$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bash_login$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bash_logoutOB$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bash_profile" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.functions$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.path" . sh-mode))
+
 (add-hook 'sh-mode-hook '(lambda()
     (development-minor-mode-hooks)
 ))
