adadc373749a08e4b8b52a7a35842fc4cd03c396
James Quigley <jmquigley@outlook.com>
2017-07-28 11:55:10 -0500

    Adds consolas 10 as a fallback font

--- .emacs.d/settings/prettify-settings.el
+++ .emacs.d/settings/prettify-settings.el
@@ -175,6 +175,8 @@
 	))
 )
 
+(set-fontset-font "fontset-default" nil (font-spec :size 10 :name "Consolas"))
+
 ;; Only use font ligatures on the mac due to terminal restrictions
 (if (string= system-type 'darwin)
 	(progn
