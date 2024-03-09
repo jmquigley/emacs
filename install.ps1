29414b18ebedde03630bebba814f40f9462c84ce
James Quigley <jmquigley@outlook.com>
2024-03-09 07:07:46 -0600

    Changed Windows powershell install script to use APPDATA

--- install.ps1
+++ install.ps1
@@ -1,5 +1,13 @@
-$src = (Get-Item -Path ".\" -Verbose).FullName + "\.emacs.d"
-$dst = "$env:USERPROFILE\.emacs.d"
+#
+# Copies the .emacs.d directory into the windows appdata roaming
+# directory to use emacas under windows
+#
 
-rm -Force -Recurse $dst
-cp -r -fo $src $dst
+$src = (Get-Item -Path ".\" -Verbose).FullName + "\.emacs.d"
+$dst = "$env:APPDATA\.emacs.d"
+
+if (test-path $dst) {
+    Remove-Item -Force -Recurse $dst
+}
+
+Copy-Item -Recurse -Force -Destination $dst $src
