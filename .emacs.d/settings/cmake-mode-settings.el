eac30ca1f39de06a52b5dd5642b511aff81a1715
jmquigley <jmquigley@outlook.com>
2025-03-29 18:57:31 -0500

    Added cmake requirements file to settings

--- .emacs.d/settings/cmake-mode-settings.el
+++ .emacs.d/settings/cmake-mode-settings.el
@@ -1,6 +1,7 @@
 (setq auto-mode-alist
       (append
        '(("CMakeLists\\.txt\\'" . cmake-mode))
+       '(("CMakeLists\\.requirements\\.txt\\'" . cmake-mode))
        '(("\\.cmake\\'" . cmake-mode))
        auto-mode-alist))
 
