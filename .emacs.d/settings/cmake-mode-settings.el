3f1838354069d07cde9196719af61ec663875836
jmquigley <jmquigley@outlook.com>
2025-03-29 16:47:47 -0500

    Added a cmake hook in settings

--- .emacs.d/settings/cmake-mode-settings.el
+++ .emacs.d/settings/cmake-mode-settings.el
@@ -4,4 +4,14 @@
        '(("\\.cmake\\'" . cmake-mode))
        auto-mode-alist))
 
+(defun base-cmake-hook ()
+"Common code for all cmake initializations"
+    (development-minor-mode-hooks)
+    (display-line-numbers-mode)
+)
+
+(add-hook 'cmake-mode-hook #'(lambda()
+    (base-cmake-hook)
+))
+
 (provide 'cmake-mode-settings)
