29aede10043199f6d4f1b26c901656f7b8d227bb
jmquigley <jmquigley@outlook.com>
2025-08-08 12:14:25 -0500

    Update claude verison

--- .emacs.d/settings/ai-settings.el
+++ .emacs.d/settings/ai-settings.el
@@ -3,7 +3,7 @@
   :key (getenv "AI_API_KEY"))
 
 (setq
- gptel-model 'claude-3-sonnet-20241022 ;  "claude-3-opus-20240229" also available
+ gptel-model 'claude-3-7-sonnet-20250219 ;  "claude-3-opus-20240229" also available
  gptel-backend (gptel-make-anthropic "Claude"
                  :stream t :key (getenv "AI_API_KEY")))
 
